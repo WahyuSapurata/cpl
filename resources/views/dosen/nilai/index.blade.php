cb1ef21e776d9bf3717c83b5f1fb9cbea34bb4c7
WahyuSapurata <wm337708@gmail.com>
2024-04-29 01:09:06 +0800

    fix

--- resources/views/dosen/nilai/index.blade.php
+++ resources/views/dosen/nilai/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.layout')
-@if (auth()->user()->role != 'kajur')
+@if (auth()->user()->role != 'kajur' && auth()->user()->role != 'operator')
     @section('button')
         <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
             <!--begin::Page title-->
@@ -83,7 +83,7 @@
                                             <th>Nilai CPMK</th>
                                             <th>Bobot</th>
                                             <th>Nilai IK</th>
-                                            @if (auth()->user()->role != 'kajur')
+                                            @if (auth()->user()->role != 'kajur' && auth()->user()->role != 'operator')
                                                 <th>Aksi</th>
                                             @endif
                                         </tr>
@@ -374,13 +374,13 @@
                             return value;
                         }
                     },
-                    @if (auth()->user()->role != 'kajur')
+                    @if (auth()->user()->role != 'kajur' && auth()->user()->role != 'operator')
                         {
                             data: 'uuid',
                         }
                     @endif
                 ],
-                @if (auth()->user()->role != 'kajur')
+                @if (auth()->user()->role != 'kajur' && auth()->user()->role != 'operator')
                     columnDefs: [{
                         targets: -1,
                         title: 'Aksi',
