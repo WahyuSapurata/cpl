87c1b6b5e26f191d1b7f6c5973e9cec639c6c3bc
WahyuSapurata <wm337708@gmail.com>
2024-08-18 21:43:49 +0800

    fix

--- resources/views/dosen/cpmk/index.blade.php
+++ resources/views/dosen/cpmk/index.blade.php
@@ -5,7 +5,19 @@
             <!--begin::Page title-->
             <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
                 data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
-                class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+                class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0 gap-3">
+                <button class="btn btn-info btn-sm" id="button-back">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" id="svg-button"
+                        viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <style>
+                            #svg-button {
+                                fill: #ffffff
+                            }
+                        </style>
+                        <path
+                            d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9L117.5 269.8c-3.5-3.8-5.5-8.7-5.5-13.8s2-10.1 5.5-13.8l99.9-107.1c4.2-4.5 10.1-7.1 16.3-7.1c12.3 0 22.3 10 22.3 22.3l0 57.7 96 0c17.7 0 32 14.3 32 32l0 32c0 17.7-14.3 32-32 32l-96 0 0 57.7c0 12.3-10 22.3-22.3 22.3c-6.2 0-12.1-2.6-16.3-7.1z" />
+                    </svg>
+                    Kembali</button>
                 <!--begin::Title-->
                 <button class="btn btn-primary btn-sm " data-kt-drawer-show="true" data-kt-drawer-target="#side_form"
                     id="button-side-form"><i class="fa fa-plus-circle" style="color:#ffffff" aria-hidden="true"></i> Tambah
@@ -154,6 +166,21 @@
 @section('script')
     <script>
         let control = new Control();
+
+        $(document).on('click', '#button-back', function() {
+            const previousUrl = document.referrer; // Ambil URL sebelumnya dari document.referrer
+
+            // Navigasi ke URL sebelumnya
+            window.location.href = previousUrl;
+
+            // Reload halaman jika sudah kembali ke URL sebelumnya
+            if (window.location.href === previousUrl) {
+                window.location.reload();
+            }
+        });
+
+
+
 
         $(document).ready(async function() {
 
